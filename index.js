218f0df1c5c68ccb624f47726e2348ac9681e228
Eyvar <eyvar31@gmail.com>
2024-02-15 22:48:43 -0500

    basics get added - solve the get query:Drops

---
+++ index.js
@@ -0,0 +1,52 @@
+import express  from "express";
+const app = express()
+
+import { BossesModel } from "./models/bosses.js";
+
+const PORT = process.env.PORT ?? 3000
+
+app.get('/bosses', async(req,res) => {
+    const result = await BossesModel.getAll()
+
+    res.json(result)
+})
+
+app.get('/bosses/:id', async(req,res) => {
+    const {id} = req.params
+    const result = await BossesModel.getById({id})
+
+    if(result == false){
+        return res.status(404).send({
+            success:false,
+            message:`Error Item Id '${id}' Not Found`
+        })
+    }
+
+    res.status(200).send({
+        success:true,
+        data:result
+    })
+})
+
+app.get('/ds', async(req,res) => {
+    const { location, drops } = req.query
+    const result = await BossesModel.getBylocationAndDrops({ location, drops})
+    if(result == false){
+        res.status(404).send({
+            success:false,
+            message:`Error Item Not Found`
+        })
+    }
+
+    res.status(200).send({
+        success:true,
+        data:result
+    })
+})
+
+
+
+
+app.listen(PORT, (req,res) => {
+    console.log(`SERVER IS LISTENING ON PORT: \n http://localhost:${PORT}`);
+})
--- models/bosses.js
+++ models/bosses.js
@@ -1,1 +1,45 @@
-import data from '../data/data.json'
+import {getJson} from '../modules/utils.js'
+const bosses = getJson('../data/data.json')
+
+export class BossesModel{
+    static async getAll(){
+        return bosses
+    }
+    static async getById({id}){
+        const findIndex = bosses.find((data) => data.id == Number(id))
+        
+        if(!id) return false
+
+        return findIndex
+
+    }
+    static async getBylocationAndDrops({location,drops}){
+				if(location){
+						const locationFound = bosses.filter((data)=> {
+								const locationUpper = data.location.toLocaleLowerCase() == location.toLocaleLowerCase()
+		
+								return locationUpper
+		
+						})
+						return locationFound
+				}
+				
+				if(drops){
+					const dropFound = bosses.filter((data) => {
+						// const dropUpper = data.drops.toLocaleLowerCase() == drops.toLocaleLowerCase()
+
+						console.log(data.Drops[1]);
+
+						// console.log(drops.toLocaleLowerCase());
+						
+						// return dropUpper
+					})
+					return dropFound
+				}
+
+
+
+
+        return location || drops
+    }
+}
