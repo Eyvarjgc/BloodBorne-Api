8692ab3c8ee12dfa69091593257fa2ab98ca06ac
Eyvar <eyvar31@gmail.com>
2024-02-20 11:56:35 -0500

    Fix: EndPoint Fixed

--- public/js/bossses.js
+++ public/js/bossses.js
@@ -3,7 +3,7 @@ const bossesSection =  document.querySelector('.bossesSection')
 
 const getApi = async() =>{
   try{
-    const response = await axios.get('http://localhost:3000/bosses') 
+    const response = await axios.get('http://localhost:3000/bosses' || 'https://bloodborne-simple-api-dev-sdqb.2.us-1.fl0.io/bosses') 
     const data = response.data.data
     console.log(data);
 
