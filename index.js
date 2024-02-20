585e104be388084f5055e5a4680f391299571412
Eyvar <eyvar31@gmail.com>
2024-02-20 09:56:11 -0500

    fix: cors fixed and new image url

--- index.js
+++ index.js
@@ -8,9 +8,30 @@ const PORT = process.env.PORT ?? 3000
 import  bossesRouter   from './routers/bosses.js'
 app.use('/bosses', bossesRouter)
 
+app.use(express.static('public'))
 
+import cors from "cors";
+// app.use(cors({
+//     origin:(origin, callback) => {
+//         const ACCEPTED_ORIGINS = [
+//             'http://localhost:8080',
+//             'http://localhost:3000',
+//             'https://log.com',
+//             'http://127.0.0.1:5500/public/bosses.html'
+//         ]
 
+//         if(ACCEPTED_ORIGINS.includes(origin)){
+//             return callback(null,true)
+//         }
+//         if(!origin){
+//             return callback(null,true)
 
+//         }
+
+//         return callback(new Error(`Not allowed by cors`))
+//     }
+// }))
+app.use(cors('*'))
 app.listen(PORT, (req,res) => {
     console.log(`SERVER IS LISTENING ON PORT: \n http://localhost:${PORT}`);
 })
